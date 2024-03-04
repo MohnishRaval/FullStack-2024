55b466ae5a5af3331c9fe4b0846f47e222404c29
Mohnish Raval <mohnishraval40@gmail.com>
2024-03-03 20:05:18 -0500

    added backend/index.js file

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// Load environment variables from .env file
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+
+// Use your environment variables
+const port = process.env.PORT || 4000;
+
+// ... rest of your Express app configuration
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
