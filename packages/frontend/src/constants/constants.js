acd0121f923bca25bed7c32bb3bd6814f03d7078
Mohnish Raval <mohnishraval40@gmail.com>
2024-03-27 21:17:58 -0400

    Added Pokemon Image URL

--- packages/frontend/src/constants/constants.js
+++ packages/frontend/src/constants/constants.js
@@ -1,5 +1,7 @@
 export const constants = {
   URLS: {
     BASE_URL: 'https://pokeapi.co/api/v2/',
+    IMG_URL:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/',
   },
 };
