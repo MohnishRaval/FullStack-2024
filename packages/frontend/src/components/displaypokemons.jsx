3a0318fe848a2110c239cd7986c1f495f170f9cc
Mohnish Raval <mohnishraval40@gmail.com>
2024-03-27 22:29:40 -0400

    Added Pokemon image Feature

--- packages/frontend/src/components/displaypokemons.jsx
+++ packages/frontend/src/components/displaypokemons.jsx
@@ -5,11 +5,9 @@ import Pokemon from './pokemon';
 import ReactPaginate from 'react-paginate';
 
 const Displaypokemons = () => {
-  const [pokemons, setPokemons] = useState([]);
   const [pokemonDetails, setPokemonDetails] = useState([]);
   const [totalPages, setTotalPages] = useState();
   const [currentPage, setCurrentPage] = useState(1);
-  console.log('New Page=', currentPage);
   useEffect(() => {
     fetchPokemons(
       constants.URLS.BASE_URL +
@@ -21,16 +19,26 @@ const Displaypokemons = () => {
     axios
       .get(url)
       .then((response) => {
-        console.log('1st API Call=', response.data.results);
-        setPokemons(response.data.results);
+        // setPokemons(response.data.results);
         setTotalPages(Math.ceil(response.data.count / 20));
-        return response.data.results.map((pokemon) => axios.get(pokemon.url));
+        return response.data.results.map((pokemon) => {
+          return axios.get(pokemon.url).then((urlResponse) => {
+            const urlResponseData = urlResponse.data;
+            return axios
+              .get(constants.URLS.IMG_URL + `${urlResponseData.id}.png`)
+              .then((imageResponse) => {
+                return {
+                  ...urlResponseData,
+                  image: imageResponse.config.url,
+                };
+              });
+          });
+        });
       })
       .then((pokemonPromises) => {
         return Promise.all(pokemonPromises);
       })
       .then((pokemonResponse) => {
-        console.log('Individual Pokemon response=', pokemonResponse);
         setPokemonDetails(pokemonResponse);
       })
       .catch((error) => {
@@ -38,22 +46,16 @@ const Displaypokemons = () => {
       });
   };
 
-  const details = pokemons.map((pokemon, index) => {
-    const pokemonDetail = pokemonDetails[index]?.data;
-    // console.log('pokemonDetail=', pokemonDetail);
+  const details = pokemonDetails.map((pokemonDetail, index) => {
     return (
       <div key={index} className="w-1/2 p-2">
         <Pokemon
           key={index}
           index={index + 1 + (currentPage - 1) * 20}
-          name={pokemon.name}
-          pokemonDetail={pokemonDetail}
+          name={pokemonDetail.name}
+          image={pokemonDetail?.image}
         ></Pokemon>
       </div>
-      // <div key={index}>
-      //   {pokemon.name}
-      //   {pokemonDetail && pokemonDetail.order}
-      // </div>
     );
   });
 
@@ -74,16 +76,18 @@ const Displaypokemons = () => {
           previousLabel={'Previous'}
           nextLabel={'Next'}
           breakLabel={'...'}
-          activeClassName={'active bg-sky-400 text-white rounded-lg'}
+          activeClassName={
+            'active bg-sky-400 hover:bg-sky-300 text-white rounded-lg'
+          }
           containerClassName={'pagination flex justify-center mt-4'}
           pageClassName={'active'}
           pageLinkClassName={
             'page-link text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
           }
           previousClassName={
-            'previous bg-amber-300 hover:bg-yellow-600 rounded-lg'
+            'previous bg-amber-300 hover:bg-yellow-100 rounded-lg'
           }
-          nextClassName={'next bg-amber-300 hover:bg-yellow-600 rounded-lg'}
+          nextClassName={'next bg-amber-300 hover:bg-yellow-100 rounded-lg'}
           previousLinkClassName={
             'previous text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
           }
--- packages/frontend/src/components/pokemon.jsx
+++ packages/frontend/src/components/pokemon.jsx
@@ -1,10 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Pokemon = ({ name, index }) => {
+const Pokemon = ({ name, index, image }) => {
   return (
     <div className="border p-1">
       {index}. {name}
-      {/* {pokemonDetail && pokemonDetail.order} */}
+      {image ? (
+        <img src={image} alt={name} className="h-20 w-20" />
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
@@ -13,5 +17,6 @@ Pokemon.propTypes = {
   name: PropTypes.string,
   pokemonDetail: PropTypes.object,
   index: PropTypes.number,
+  image: PropTypes.string,
 };
 export default Pokemon;
