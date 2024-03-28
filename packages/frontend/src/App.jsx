b5cfe07903dfae4419cfb1ba2a49243481627c8c
Mohnish Raval <mohnishraval40@gmail.com>
2024-03-28 11:51:29 -0400

    Added Pokemon Search

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import Displaypokemons from './components/displaypokemons.jsx';
+import { Displaypokemons } from './components/displaypokemons.jsx';
 // import { Navbar } from './components/navbar.jsx';
+import { Searchpokemon } from './components/searchpokemon.jsx';
 
 const App = () => {
   return (
     <div className="App h-full">
       {/* <Navbar></Navbar> */}
+      <Searchpokemon></Searchpokemon>
       <Displaypokemons></Displaypokemons>
     </div>
   );
---
+++ packages/frontend/src/components/searchpokemon.jsx
@@ -0,0 +1,14 @@
+export const Searchpokemon = () => {
+  return (
+    <div
+      className="parent-searchpokemon flex
+     w-full py-4"
+    >
+      <input
+        type="search"
+        placeholder="Search Pokemon"
+        className="mx-auto flex size-full h-10 w-1/2 justify-center rounded-lg border-2 border-black p-1"
+      />
+    </div>
+  );
+};
