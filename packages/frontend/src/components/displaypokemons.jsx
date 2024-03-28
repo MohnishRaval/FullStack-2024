dc56c4168dd43a9c4424ba9b5084bb5ebbedc67d
Mohnish Raval <mohnishraval40@gmail.com>
2024-03-27 21:17:46 -0400

    Updated CSS for Display Pokemons

--- packages/frontend/src/components/displaypokemons.jsx
+++ packages/frontend/src/components/displaypokemons.jsx
@@ -23,9 +23,7 @@ const Displaypokemons = () => {
       .then((response) => {
         console.log('1st API Call=', response.data.results);
         setPokemons(response.data.results);
-        setTotalPages(
-          Math.ceil(response.data.count / response.data.results.length),
-        );
+        setTotalPages(Math.ceil(response.data.count / 20));
         return response.data.results.map((pokemon) => axios.get(pokemon.url));
       })
       .then((pokemonPromises) => {
@@ -44,11 +42,14 @@ const Displaypokemons = () => {
     const pokemonDetail = pokemonDetails[index]?.data;
     // console.log('pokemonDetail=', pokemonDetail);
     return (
-      <Pokemon
-        key={index}
-        name={pokemon.name}
-        pokemonDetail={pokemonDetail}
-      ></Pokemon>
+      <div key={index} className="w-1/2 p-2">
+        <Pokemon
+          key={index}
+          index={index + 1 + (currentPage - 1) * 20}
+          name={pokemon.name}
+          pokemonDetail={pokemonDetail}
+        ></Pokemon>
+      </div>
       // <div key={index}>
       //   {pokemon.name}
       //   {pokemonDetail && pokemonDetail.order}
@@ -63,32 +64,34 @@ const Displaypokemons = () => {
   };
 
   return (
-    <div className="parent-displaypokemons container border p-4">
-      <div className="pokemon-details p-2">{details}</div>
-      <ReactPaginate
-        className="pagination flex justify-center space-x-4 p-3 font-bold"
-        pageCount={totalPages}
-        onPageChange={handlePageChange}
-        previousLabel={'Previous'}
-        nextLabel={'Next'}
-        breakLabel={'...'}
-        activeClassName={'active bg-blue-500 text-white rounded-lg'}
-        containerClassName={'pagination flex justify-center mt-4'}
-        pageClassName={'active'}
-        pageLinkClassName={
-          'page-link text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
-        }
-        previousClassName={
-          'previous bg-yellow-500 hover:bg-yellow-600 rounded-lg'
-        }
-        nextClassName={'next bg-yellow-500 hover:bg-yellow-600 rounded-lg'}
-        previousLinkClassName={
-          'previous text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
-        }
-        nextLinkClassName={
-          'next text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
-        }
-      ></ReactPaginate>
+    <div className="parent-displaypokemons container p-4">
+      <div className="pokemon-details flex flex-wrap p-2">{details}</div>
+      <div className="pagination fixed bottom-0 left-0 right-0 border bg-gray-600">
+        <ReactPaginate
+          className="pagination m-auto flex justify-center space-x-16 p-3 font-bold"
+          pageCount={totalPages}
+          onPageChange={handlePageChange}
+          previousLabel={'Previous'}
+          nextLabel={'Next'}
+          breakLabel={'...'}
+          activeClassName={'active bg-sky-400 text-white rounded-lg'}
+          containerClassName={'pagination flex justify-center mt-4'}
+          pageClassName={'active'}
+          pageLinkClassName={
+            'page-link text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
+          }
+          previousClassName={
+            'previous bg-amber-300 hover:bg-yellow-600 rounded-lg'
+          }
+          nextClassName={'next bg-amber-300 hover:bg-yellow-600 rounded-lg'}
+          previousLinkClassName={
+            'previous text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
+          }
+          nextLinkClassName={
+            'next text-base text-gray-900 dark:text-black w-8 h-8 mr-1 flex justify-center items-center w-full p-1'
+          }
+        ></ReactPaginate>
+      </div>
     </div>
   );
 };
